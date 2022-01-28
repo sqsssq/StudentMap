e5bd3b88fc348f483768cacc6bda0956971ee665
SanZhi <1628245977@qq.com>
2022-01-28 14:39:04 +0800

    Covid-19_in_Hangzhou

---
+++ .history/realworld.388_20220128121448.js
@@ -0,0 +1,10 @@
+
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+var addressPoints = [
+    // [-37.8210922667, 175.2209316333, "2"],
+    [30.084461, 120.185076, "1"],
+    
+    // [,, ""]
+    [30.183715, 120.212423, "2"]
+    ];
